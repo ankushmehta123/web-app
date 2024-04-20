273f687fa70dd625b3a96be13440004b93bddaa7
Ankush <32393931+ankushmehta123@users.noreply.github.com>
2024-04-20 18:07:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
-function App() {
-  const name = 'uwu';
+const Person = (props) => {
+  return (
+    <>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h2>Age: {props.Age}</h2>
+    </>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <h1>Hi {name}</h1>
+      
+      <Person name={'Ankush'} lastName={'Mehta'} Age={70} />
+
     </div>
   );
 }
